5f0bbb6e133bc9245798606fd1a89ea833996cf5
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-09-17 23:03:15 +0530

    ds

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://final-year-eta.vercel.app/']
+const allowedOrigins = ['http://localhost:5173', 'https://final-year-eta.vercel.app']
 
 app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 

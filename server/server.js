8cf1e58043410a81f254254d68dd499613e5ded7
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-09-17 13:56:22 +0530

    ds

--- server/server.js
+++ server/server.js
@@ -19,14 +19,14 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', '']
+const allowedOrigins = ['http://localhost:5173', 'https://final-year-eta.vercel.app/']
 
-app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
+app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 
 // Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 
 app.get('/', (req, res) => res.send("API is Working"));
@@ -37,6 +37,6 @@ app.use('/api/cart', cartRouter)
 app.use('/api/address', addressRouter)
 app.use('/api/order', orderRouter)
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
